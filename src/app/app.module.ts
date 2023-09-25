0573648a5b60e7257d6e40889c01b14cd3f82a6f
Hubert Kieruczenko <Hub3rt65@gmail.com>
2023-09-25 14:02:34 +0200

    global style improvement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { WrongPathComponent } from './wrong-path/wrong-path.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, WrongPathComponent],
--- src/app/user-zone/crypto/crypto.module.ts
+++ src/app/user-zone/crypto/crypto.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { CryptoRoutingModule } from './crypto-routing.module';
 import { CryptoListComponent } from './crypto-list/crypto-list.component';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [CryptoListComponent],
-  imports: [CommonModule, CryptoRoutingModule, MatProgressSpinnerModule]
+  imports: [
+    CommonModule,
+    CryptoRoutingModule,
+    MatProgressSpinnerModule
+  ]
 })
 export class CryptoModule {}
